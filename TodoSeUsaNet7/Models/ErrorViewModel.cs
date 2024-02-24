7ac9b3316e797c3090744ef7b09bdb8855f714ae
Abstr4 <matias.rojas@mi.unc.edu.ar>
2024-02-24 11:37:31 -0300

    Add project files.

---
+++ TodoSeUsaNet7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoSeUsaNet7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
