4761e759042217a4afe2891194c9284f46c6b781
Abstr4 <matias.rojas@mi.unc.edu.ar>
2024-02-26 09:06:40 -0300

    Scaffolded Login and Register with Identity

---
+++ TodoSeUsaNet7/Areas/Identity/Data/TodoSeUsaNet7User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TodoSeUsaNet7.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the TodoSeUsaNet7User class
+public class TodoSeUsaNet7User : IdentityUser
+{
+}
+
--- TodoSeUsaNet7/Program.cs
+++ TodoSeUsaNet7/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TodoSeUsaNet7.Areas.Identity.Data;
+using TodoSeUsaNet7.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TodoSeUsaNet7ContextConnection") ?? throw new InvalidOperationException("Connection string 'TodoSeUsaNet7ContextConnection' not found.");
+
+builder.Services.AddDbContext<TodoSeUsaNet7Context>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<TodoSeUsaNet7User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<TodoSeUsaNet7Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -23,5 +33,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
