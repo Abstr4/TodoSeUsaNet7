61fbd2047c9703ad7fd3726129c691bdfe3d2f0f
Abstr4 <matias.rojas@mi.unc.edu.ar>
2024-02-26 10:47:11 -0300

    User FirstName and LastName properties

--- TodoSeUsaNet7/Areas/Identity/Data/TodoSeUsaNet7User.cs
+++ TodoSeUsaNet7/Areas/Identity/Data/TodoSeUsaNet7User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,12 @@ namespace TodoSeUsaNet7.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the TodoSeUsaNet7User class
 public class TodoSeUsaNet7User : IdentityUser
 {
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; }
 }
 
