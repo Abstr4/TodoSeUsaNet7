e97ab2bb53be4536867127146d98df257768e02d
Abstr4 <matias.rojas@mi.unc.edu.ar>
2024-02-27 17:57:55 -0300

    Initial Migration

--- TodoSeUsaNet7/Migrations/20240226140422_Initial Create.Designer.cs
+++ TodoSeUsaNet7/Migrations/20240226140422_Initial Create.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using TodoSeUsaNet7.Data;
+using TodoSeUsaNet7.Models;
 
 #nullable disable
 
--- TodoSeUsaNet7/Migrations/TodoSeUsaNet7ContextModelSnapshot.cs
+++ TodoSeUsaNet7/Migrations/TodoSeUsaNet7ContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using TodoSeUsaNet7.Data;
+using TodoSeUsaNet7.Models;
 
 #nullable disable
 
