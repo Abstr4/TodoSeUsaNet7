46ad29a573a4f8ddece5430db92e4a91cc78045e
Abstr4 <matias.rojas@mi.unc.edu.ar>
2024-07-10 18:33:47 -0300

    merge fix

--- TodoSeUsaNet7/Program.cs
+++ TodoSeUsaNet7/Program.cs
@@ -4,18 +4,11 @@ using TodoSeUsaNet7.Models.Data;
 using TodoSeUsaNet7.Models.Seeding;
 using TodoSeUsaNet7.Models.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
-<<<<<<< HEAD
 // Get the connection string from the environment variable
 var connectionString = Environment.GetEnvironmentVariable("TODOSEUSANET7_CONNECTION_STRING_DEMO");
 /*                       ?? Environment.GetEnvironmentVariable("TODOSEUSANET7_CONNECTION_STRING");
 */
-=======
-
-// Get the connection string from the environment variable
-var connectionString = Environment.GetEnvironmentVariable("TODOSEUSANET7_CONNECTION_STRING");
->>>>>>> master
 
 builder.Services.AddDbContext<TodoSeUsaNet7Context>(options => options.UseSqlServer(connectionString));
 
@@ -57,17 +50,15 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<TodoSeUsaNet7Context>();
-<<<<<<< HEAD
 
     // Applies any pending migrations
     dbContext.Database.Migrate(); 
 
     // Seed data
     await DataSeeder.SeedDataAsync(dbContext);
-=======
     // Applies any pending migrations
     dbContext.Database.Migrate();
->>>>>>> master
+
 }
 
 
